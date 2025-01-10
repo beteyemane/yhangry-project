ec1a6bd63d5455cf466b1d4057febb83ed4c878a
beteyemane <beteyemane@hotmail.co.uk>
2025-01-10 16:42:49 +0000

    server

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const setMenusRoutes = require('./routes/setMenus');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/api', setMenusRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
